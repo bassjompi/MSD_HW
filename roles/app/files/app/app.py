d53b70da416516bd5e509e4a36b1b55682c564d2
juan pablo yanez <bassjompi@gmail.com>
2018-04-11 09:21:10 +0000

    changed the app to show the ip of Eth1 instead the loopback

--- roles/app/files/app/app.py
+++ roles/app/files/app/app.py
@@ -2,21 +2,32 @@ from flask import Flask, render_template
 import psutil
 import time
 import socket
-
+import fcntl
+import struct
 
 application = Flask(__name__)
 
 @application.route('/')
 def systeminfo():
+
     usage=str("CPU USAGE  {}%".format(psutil.cpu_percent())+"\n")
     tiempo= time.strftime('%H:%M:%S %Z on %b %d, %Y')
-    ip=(socket.gethostbyname(socket.gethostname()))
 
-    numbers = list(ip)
+    def get_ip_address(ifname):                                         ## function to obtain ip of a certain device
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,                                                      # SIOCGIFADDR
+            struct.pack('256s', ifname[:15])
+        )[20:24])
+
+    ip= get_ip_address('eth1')
+    numbers = list(ip)                                                  ##creates a list with the ip address
+
     odd = []
     even = []
-    for i in numbers:
-        if i != '.':
+    for i in numbers:                                                    ##check if numbers are odd or even
+        if i != '.':            
             cifra = int(i)
             if cifra % 2 == 0:
                 even.append(i)
